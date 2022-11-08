3c4989a04c1e08ada55d9e93bea19677cb302516
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-11-08 21:31:46 +0300

    CreateSensor component init.

---
+++ src/app/create-sensor/create-sensor.component.ts
@@ -0,0 +1,22 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IS_NEW_SENSOR } from 'src/env.consts';
+
+@Injectable()
+@Component({
+  selector: 'app-create-sensor',
+  templateUrl: './create-sensor.component.html',
+  styleUrls: ['./create-sensor.component.css']
+})
+export class CreateSensorComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  public goToSensorForm() {
+    sessionStorage.setItem(IS_NEW_SENSOR, true.toString());
+    this.router.navigate(['/sensor-form']);
+  }
+}
