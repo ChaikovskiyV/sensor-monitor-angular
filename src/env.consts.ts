ae5752ae36638b8658ee60b5c365cf0d7497fe78
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-11-11 00:07:21 +0300

    Env constants init

---
+++ src/env.consts.ts
@@ -0,0 +1,5 @@
+export const JWT_TOKEN_KEY = 'jwtToken';
+export const USERNAME_KEY = 'username';
+export const AUTHORITIES_KEY = 'authorities';
+export const IS_NEW_SENSOR_KEY = 'isNewSensor';
+export const CURRENT_SENSOR_ID_KEY = 'currentSensorId';
