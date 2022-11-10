6ea9f52f74452e0b8ef82842502fb9a4eaecdded
ChaikovskiyV <chaikovskiy.va@mail.ru>
2022-11-11 00:06:34 +0300

    App-routing module init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { SensorFormComponent } from './sensor-form/sensor-form.component';
+import { SensorDataComponent } from './sensor-data/sensor-data.component';
 import { SensorsComponent } from './sensors/sensors.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'sensors', component: SensorsComponent },
-  { path: 'sensor', component: SensorFormComponent },
+  { path: 'sensor-form', component: SensorDataComponent },
   { path: '', redirectTo: '/login', pathMatch:'full' }
 ];
 
